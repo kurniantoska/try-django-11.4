e49d6650313ebd684c2bac5ac98dac6d021a525b
skater <alternative.xen@gmail.com>
2017-08-16 11:25:06 +0800

    initial commit again

--- skaterapp/skaterapp/settings/base.py
+++ skaterapp/skaterapp/settings/base.py
@@ -13,14 +13,14 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd&j$@9i#_=f=spzta@=efg&y@5in4f1zue_3gzk+he)1j)d-uh'
+SECRET_KEY = 'xx2$@9i#_=f=spzta@=efg&y@5in4f1zue_3gzk+he)1j)d-uh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restaurant',
+    'restaurants',
 ]
 
 MIDDLEWARE = [
