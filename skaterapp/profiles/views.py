6398cd0d9540ba6b432eba72d8faaed79976849d
kurniantoska <alternative.xen@gmail.com>
2017-09-26 07:54:25 +0000

    next level user add profile

--- skaterapp/profiles/views.py
+++ skaterapp/profiles/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from django.contrib.auth import get_user_model
-from django.views.generic import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View, DetailView
 
 from menus.models import Item
 from restaurants.models import RestaurantLocation
 # Create your views here.
 
+from .models import Profile
+
 User = get_user_model()
+
+
+class ProfileFollowToggle(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        
+        user_to_toggle = request.POST.get("username")
+        print("{} - variabel tootle".format(user_to_toggle))
+        profile_ = Profile.objects.get(user__username__iexact=user_to_toggle)
+        user = request.user
+        if user in profile_.followers.all():
+            profile_.followers.remove(user)
+        else:
+            profile_.followers.add(user)
+        return redirect('/u/skater/')
+
+
 class ProfileDetailView(DetailView):
     def get_object(self):
         username = self.kwargs.get("username")
@@ -15,9 +34,13 @@ class ProfileDetailView(DetailView):
             raise Http404
         return get_object_or_404(User, username__iexact=username, is_active=True)
 
-    def get_context_data(self, **kwargs):
-        context = super(ProfileDetailView, self).get_context_data(**kwargs)
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProfileDetailView, self).get_context_data(*args, **kwargs)
         user = context['user']
+        is_following = False
+        if user.profile in self.request.user.is_following.all():
+            is_following = True
+        context['is_following'] = is_following
         query = self.request.GET.get('q')
         item_exists = Item.objects.filter(user=user).exists()
         qs = RestaurantLocation.objects.filter(owner=user).search(query)
--- skaterapp/skaterapp/urls.py
+++ skaterapp/skaterapp/urls.py
@@ -19,10 +19,14 @@ from django.views.generic import TemplateView
 
 from django.contrib.auth.views import LoginView
 
+from profiles.views import ProfileFollowToggle
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^profile-follow/$', ProfileFollowToggle.as_view(), name='follow'),
     url(r'^u/', include('profiles.urls', namespace='profile')),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^items/', include('menus.urls', namespace='menus')),
