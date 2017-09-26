07de9fd8c9f85267484b4a736b37c58704270b8a
Riska K A <alternative.xen@gmail.com>
2017-09-26 05:02:23 +0000

    final profile database

--- skaterapp/profiles/models.py
+++ skaterapp/profiles/models.py
@@ -7,8 +7,8 @@ User = settings.AUTH_USER_MODEL
 # Create your models here.
 class Profile(models.Model) :
     user             = models.OneToOneField(User)
-    followers        = models.ManyToManyField(User, related_name='followers', blank=True)
-    following        = models.ManyToManyField(User, related_name='following', blank=True)
+    followers        = models.ManyToManyField(User, related_name='is_following', blank=True)
+    #following        = models.ManyToManyField(User, related_name='following', blank=True)
     activated        = models.BooleanField(default=False)
     timestamp        = models.DateField(auto_now_add=True)
     updated          = models.DateField(auto_now=True)
@@ -20,5 +20,10 @@ class Profile(models.Model) :
 def post_save_user_receiver(sender, instance, created, *args, **kwargs) :
     if created:
         profile, is_created = Profile.objects.get_or_create(user=instance)
+        default_user_profile = Profile.objects.get_or_create(user__id=1)[0]
+        default_user_profile.followers.add(instance)
+        #default_user_profile.save()
+        profile.followers.add(default_user_profile.user)
+        profile.followers.add(2)
         
 post_save.connect(post_save_user_receiver, sender=User)
